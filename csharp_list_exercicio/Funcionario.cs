77b636f11024743fd5ea9ae5265115d7c8cbb964
Plastyc <neverax123@gmail.com>
2024-04-23 10:04:10 -0300

    Adicionar arquivos de projeto.

---
+++ csharp_list_exercicio/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace csharp_list_exercicio
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public int Id { get; private set; }
+        public double Salario { get; private set; }
+
+        
+        public Funcionario(string nome, int id, double salario)
+        {
+            Nome = nome;
+            Id = id;
+            Salario = salario;
+        }
+
+        public void AumentaSalario(double porcentagem)
+        {
+            Salario += Salario * (porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return Id + ", "
+                + Nome + ", "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ csharp_list_exercicio/Program.cs
@@ -0,0 +1,40 @@
+﻿using csharp_list_exercicio;
+using System.Globalization;
+
+Console.Write("Quantos funcionários você quer cadastrar? ");
+int resp = int.Parse(Console.ReadLine());
+
+List<Funcionario> funcionarios = new List<Funcionario>();
+
+for (int i = 0; i < resp; i++)
+{
+    Console.Write("Insira o id do funcionário: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Insira o nome do funcionário: ");
+    string nome = Console.ReadLine();
+    Console.Write("Insira o salário do funcionário: ");
+    double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    funcionarios.Add(new Funcionario(nome, id, salario));
+    Console.WriteLine("==================================\n");
+}
+foreach (Funcionario obj in funcionarios)
+{
+    Console.WriteLine(obj);
+}
+
+Console.Write("Insira o Id do funcionário do qual você quer aumentar o salário: ");
+int idSearch = int.Parse(Console.ReadLine());
+Console.Write("Qual a porcentagem de aumento? ");
+double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+if(funcionarios.Find(x => x.Id == idSearch) != null)
+{
+    funcionarios.Find(x => x.Id == idSearch).AumentaSalario(porcentagem);
+} else
+{
+    Console.WriteLine("Id inválido");
+}
+Console.WriteLine("Dados atualizados: ");
+foreach (Funcionario obj in funcionarios)
+{
+    Console.WriteLine(obj);
+}
